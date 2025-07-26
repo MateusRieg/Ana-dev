f7b6e87ca9eb2e3c43895bbc36a627ad98405109
Mateus Rieg <mateusrieg@wifeed.com.br>
2025-07-26 16:24:54 -0300

    initial COmmit 2

--- src/main/java/br/com/ana/AppInit.java
+++ src/main/java/br/com/ana/AppInit.java
@@ -1,16 +1,62 @@
 package br.com.ana;
 
-import br.com.ana.model.Cliente;
+import br.com.ana.model.Pessoa;
 
-import java.util.Scanner;
+import java.util.*;
 
 public class AppInit {
 
     public static void main(String[] args) {
-        Cliente cliente = new Cliente();
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Insira seu nome: ");
-        String name = sc.nextLine();
-        System.out.println("seu nome é :" + name);
+
+        List<Pessoa> pessoas = new ArrayList<>();
+        boolean program = true;
+        while(program) {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("********** Bem vindo ao sistema de cadastro de pessoas **************");
+            System.out.println("Selecione a opção desejada:");
+            System.out.println("1- Adicionar pessoa:");
+            System.out.println("2- Remover pessoa:");
+            System.out.println("3- Listar pessoa:");
+            System.out.println("4 - Sair:");
+            int opcao = sc.nextInt();
+            switch (opcao) {
+                case 1:
+                    if(!pessoas.isEmpty()) {
+                        System.out.println("Maximo de pessoas ja cadastradas.");
+                        break;
+                    }
+                    System.out.println("Insira seu nome: ");
+                    String name = sc.next();
+                    System.out.println("Agora Diga sua idade: ");
+                    int idade = sc.nextInt();
+
+                    Pessoa pessoa1 = new Pessoa();
+                    pessoa1.setNome(name);
+                    pessoa1.setIdade(idade);
+                    pessoas.add(pessoa1);
+                    break;
+                case 2:;
+                case 3:
+                    System.out.println("**** Lista de pessoas Cadastradas ****");
+                    for (Pessoa ps : pessoas) {
+                        System.out.println("Nome: " + ps.getNome() );
+                        System.out.println("Nome: " + ps.getIdade() );
+                        System.out.println(" ");
+                    }
+                    break;
+                case 4:
+                    program = false;
+                    break;
+                default:
+                    throw new IllegalStateException("Essa opção não é valida: " + opcao);
+            }
+        }
     }
+
+    private static void verificarIdade(int idade) {
+        if(idade > 20) {
+            System.out.println("Você é velho");
+        }
+    }
+
 }
